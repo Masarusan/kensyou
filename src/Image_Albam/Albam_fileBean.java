14c4fa6f28da3580a0d6c647c378e0112afae400
masarusan <japonica918@gmail.com>
2017-11-22 02:04:33 +0900

    テスト

--- src/Image_Albam/Albam_fileBean.java
+++ src/Image_Albam/Albam_fileBean.java
@@ -1,14 +1,79 @@
 package Image_Albam;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
-public class Albam_file {
+import javax.imageio.ImageIO;
 
+public class Albam_fileBean {
+	private BufferedImage image;
+	private Image Image_Read;
+
+	public BufferedImage getImage() {
+		return image;
+	}
+	public BufferedImage setImage(BufferedImage image) {
+		return this.image = image;
+	}
+	public Image getImage_Read() {
+		return Image_Read;
+	}
+	public void setImage_Read(Image image_Read) {
+		Image_Read = image_Read;
+	}
 	//ファイル読み込み
+	public Path file_read(Path path) {
+		try(InputStream is = Files.newInputStream(path, StandardOpenOption.CREATE)) {
+			for(int ch;(ch = is.read())!= -1;) {
+				System.out.println(ch);
+			}
+		}
+		catch(IOException ex) {
+			System.err.println(ex);
+		}
+		return path;
+
+	}
+	//inputstream変換
+	public BufferedImage image_read(BufferedImage image) {
+		// 対象となるBufferedImage
+		//BufferedImage image;
+		// 一度byte配列へ変換
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			image = ImageIO.read(new File("image.jpg"));
+			ImageIO.write( image, ".jpg", baos );
+		byte[] imageInByte = baos.toByteArray();
+		// byte配列をInputStreamに変換
+		InputStream in = new ByteArrayInputStream(imageInByte);
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return this.setImage(image);
+
+	}
+	//画像読み込み
+	public BufferedImage image_reader(BufferedImage readImage, Path path) {
+		try {
+		  readImage = ImageIO.read(path.toFile());
+		} catch (Exception e) {
+		  e.printStackTrace();
+		  readImage = null;
+		}
+		return this.setImage(readImage);
+
+	}
 	//ファイル書き込み
 	//ファイル作成
 	//ファイル名入力
